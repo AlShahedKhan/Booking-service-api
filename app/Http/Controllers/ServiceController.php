3e3ac04a7671fa8e04f15715a6efe99b25da27c4
Abdullah al shahed <abdullahalshahedkhan@gmail.com>
2025-07-30 19:16:06 +0600

    service controller implemented

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ServiceRequest;
+use App\Interfaces\ServiceRepositoryInterface;
+
+class ServiceController extends Controller
+{
+    protected $serviceRepo;
+    public function __construct(ServiceRepositoryInterface $serviceRepo) {
+        $this->serviceRepo = $serviceRepo;
+    }
+
+    public function index() { return response()->json($this->serviceRepo->all()); }
+    public function store(ServiceRequest $request) {
+        return response()->json($this->serviceRepo->create($request->validated()), 201);
+    }
+    public function update(ServiceRequest $request, $id) {
+        return response()->json($this->serviceRepo->update($id, $request->validated()));
+    }
+    public function destroy($id) {
+        $this->serviceRepo->delete($id);
+        return response()->json(null, 204);
+    }
+}
