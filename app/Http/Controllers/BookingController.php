a18262fca9689a5dc05650e89a6f7cfbc842dfaf
Abdullah al shahed <abdullahalshahedkhan@gmail.com>
2025-07-30 19:16:56 +0600

    booking controller created

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BookingRequest;
+use App\Interfaces\BookingRepositoryInterface;
+use Illuminate\Support\Facades\Auth;
+
+class BookingController extends Controller
+{
+    protected $bookingRepo;
+    public function __construct(BookingRepositoryInterface $bookingRepo) {
+        $this->bookingRepo = $bookingRepo;
+    }
+
+    public function index() {
+        $userId = Auth::id();
+        return response()->json($this->bookingRepo->userBookings($userId));
+    }
+
+    public function store(BookingRequest $request) {
+        $data = $request->validated();
+        $data['user_id'] = Auth::id();
+        return response()->json($this->bookingRepo->create($data), 201);
+    }
+}
